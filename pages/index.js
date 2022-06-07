9e7f44d4607a27655908213731d7ca56ed3244bc
jeferson <informatica@camarabatatais.sp.gov.br>
2022-06-07 11:39:59 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return ( 
         <div>
-            <h1>Home</h1> 
+            <h1>Home updated (nova versão em produção...)</h1> 
             <Contador />
         </div>
     )    
