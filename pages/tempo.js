cd3184d449b095e468a94241b9c2928910e5b03e
jeferson <informatica@camarabatatais.sp.gov.br>
2022-06-08 08:38:08 -0300

    adicionando delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,20 +1,23 @@
 
 
 function Tempo(props) {
-    console.log('Passando pelo Frontend');
+    console.log('> Passando pelo Frontend');
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
         </div>
     )
 }
 
-export function getStaticProps(){
-    console.log('Passando pelo getStaticProps();');
+export async function getStaticProps(){
+    console.log('> Passando pelo getStaticProps();');
+    console.log('> Adicionando delay de 5s');
+    
+    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
@@ -24,5 +27,7 @@ export function getStaticProps(){
         }
 }
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
