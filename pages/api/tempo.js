047d90b09ba1d51cbcde91f6ffce733bd8af9957
jeferson <informatica@camarabatatais.sp.gov.br>
2022-06-10 08:56:35 -0300

    convertkit_api_secret2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -7,6 +7,8 @@ async function tempo(request, response){
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers;
 
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
         date: dynamicdate.toGMTString(),
         inscritos: inscritos
